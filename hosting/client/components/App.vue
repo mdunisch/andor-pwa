461a916732111ca55544eb4f3f3672f6ebd9cbf0
mdunisch <subseven7>
2019-02-20 09:56:02 +0100

    adding keen-ui modual import

--- hosting/client/components/App.vue
+++ hosting/client/components/App.vue
@@ -1,13 +1,33 @@
 <template>
   <div>
-    App loaded
+    <ui-toolbar title="Inbox">
+      <div slot="actions">
+        <ui-icon-button color="black" icon="refresh" size="large" type="secondary"></ui-icon-button>
+
+        <ui-icon-button color="black" icon="search" size="large" type="secondary"></ui-icon-button>
+
+        <ui-icon-button
+          color="black"
+          has-dropdown
+          icon="more_vert"
+          ref="dropdownButton1"
+          size="large"
+          type="secondary"
+        >
+          <ui-menu
+            contain-focus
+            has-icons
+            slot="dropdown"
+            :options="menuOptions"
+            @close="$refs.dropdownButton1.closeDropdown()"
+          ></ui-menu>
+        </ui-icon-button>
+      </div>
+    </ui-toolbar>
   </div>
 </template>
 
 <script>
-
-
-
 export default {
   methods: {
     openGoogleForms() {
--- hosting/client/index.js
+++ hosting/client/index.js
@@ -1,5 +1,11 @@
 import Vue from 'vue';
 
+import { UiToolbar, UiIconButton, UiMenu } from 'keen-ui';
+
+Vue.component('ui-toolbar', UiToolbar);
+Vue.component('ui-icon-button', UiIconButton);
+Vue.component('ui-menu', UiMenu);
+
 import App from './components/App';
 import store from './store';
 
