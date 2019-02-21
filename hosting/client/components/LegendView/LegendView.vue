eea0fadd9f020ef134eabac2b9dfaf5aeb3baac8
mdunisch <subseven7>
2019-02-21 13:25:57 +0100

    adding card-buttons

--- hosting/client/components/LegendView/LegendView.vue
+++ hosting/client/components/LegendView/LegendView.vue
@@ -1,13 +1,32 @@
 <template>
   <div>
     <h3>{{ legendName }}</h3>
-    <ul>
-      <li v-for="card in cards.filter(i => i.type ==='letter')" :key="card.name">{{ card.name }}</li>
-    </ul>
+    <div style="display: flex;">
+      <ui-button
+        v-for="card in cards.filter(i => i.type ==='letter')"
+        :key="card.name"
+        :raised="!card.seen"
+        size="large"
+        class="cardbtn"
+        :type="card.seen ? 'primary': 'secondary'"
+        @click="opencard(card.name)"
+      >{{ card.name }}</ui-button>
+    </div>
     <hr>
-    <ul>
-      <li v-for="card in cards.filter(i => i.type ==='custom')" :key="card.name">{{ card.name }}</li>
-    </ul>
+    <div style="display: flex;">
+      <ui-button
+        v-for="card in cards.filter(i => i.type ==='custom')"
+        :key="card.name"
+        :raised="!card.seen"
+        size="large"
+        :type="card.seen ? 'primary': 'secondary'"
+        class="cardbtn customcard"
+        @click="opencard(card.name)"
+      >
+        <strong>{{ card.name }}</strong>
+        <small>{{ card.subname }}</small>
+      </ui-button>
+    </div>
   </div>
 </template>
 
@@ -20,9 +39,31 @@ export default {
     legendName() {
       return this.$store.getters.currentLegend.name;
     }
+  },
+  methods: {
+    opencard(name){
+      return this.$store.commit('openCard', name);
+    }
   }
 };
 </script>
 
 <style scoped>
+.cardbtn {
+  margin: 20px;
+}
+
+.customcard {
+  max-width: 200px;
+  height: auto;
+  padding: 15px;
+}
+
+.customcard >>> .ui-button__content {
+  flex-direction: column;
+}
+
+.customcard >>> .ui-button__content strong {
+  padding-bottom: 20px;
+}
 </style>
--- hosting/client/store.js
+++ hosting/client/store.js
@@ -18,16 +18,19 @@ export default new Vuex.Store({
           {
             type: 'letter',
             name: 'A1',
+            seen: false,
             text: 'Führt zuerst die Anweisungen auf der **Checkliste** aus.\n\nAnschließend legt ihr zusätzlich folgenden Material neben dem Spielplan bereit:\n* Legt die Hexte, die 15 Kreaturplättchen, den Gegenstand Gibt und 3 schwarze Würfel bereit.\n* Legen **Sternchen auf D, H und N** der Legendenleiste.\n\n*In Andor trug ist eine Geschichte zu, die Ging so:....*\n\nStellt alle Helden auf das Feld **57.**\n\n***\n**Aufgabe:**\nDie Helden müssen die Burg verteidigen und den **die Dunkelheit** daran hindern, alle Bauern zu verschlingen bevor der Erzähler den Buchstaben **Feld H** erreicht hat.\n***',
           },
           {
             type: 'letter',
             name: 'A2',
+            seen: false,
             text: 'Jetzt gehts los!',
           },
           {
             type: 'custom',
             name: 'In der Dunkelheit',
+            seen: false,
             subname: 'Diese Karte wird aufgedeckt, sobald die Dunkelheit in der Burg ist',
             text: '*Nach dem Sieg über den Drachen Tarok ist es Varkur gelungen, ein Drachenei in seinen Besitz zu bringen. Einen zweiten Drachen würde Andor nicht überleben, doch glücklicherweise konnten die Helden das Drachenei stehlen. Doch die eigentliche Aufgabe fängt gerade erst an: Um das Drachenei zu zerstören, müssen die Helden mehr altes Wissen über die Drachen zusammentragen und gleichzeitig den Bewohnern Andors helfen, sich von Taroks Angriff zu erholen.*\n\n***\n**Legendenziel:**\nAlle Feld um die Brug (Feld 22, 34, 45, 34 und 33) müssen von der Dunkelheit befreit sein.\n***\n**Wichtig:** Für jeden Feld, das ein Held die Dunkelheit bekämpft, muss der Held Willenspunkte abgeben:\nbei **2** Spielern **pro Feld 2 Willenspunkte**\nbei **3** Spielern **pro Feld 3 Willenspunkte**\nbei **4** Spielern **pro Feld 4 Willenspunkte**\n',
           }
@@ -44,8 +47,12 @@ export default new Vuex.Store({
     openlegend(state, legendSlug) {
       state.ui.openlegend = legendSlug;
     },
-    closelegend(state,){
-      state.ui.openlegend = false;
+    openCard(state, cardname){
+      state.legends
+      .find(i => i.slug === state.ui.openlegend)
+      .cards
+      .find(cards => cards.name === cardname)
+      .seen = true;
     }
   },
   getters: {
